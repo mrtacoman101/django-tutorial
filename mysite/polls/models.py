f493f6f1a272507a546b66c31246f13cc3cfab5c
FerreiraA <FerreiraA@knights.local>
2019-05-07 09:13:24 -0400

    added questions

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Question(models. Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
+    pub_date = models.DateTimeField('date published', blank = True)
+    img = models.URLField(max_length=200, blank = True)
     def __str__(self):
         return self.question_text
 
@@ -13,7 +13,7 @@ class Question(models. Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-
+    
     votes = models.IntegerField(default=0)
 
     def __str__(self):
